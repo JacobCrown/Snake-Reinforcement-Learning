fce07e20e95b12c35d137c9e03cfa7f38de37bb6
JacobCrown <jkorona.pl@gmail.com>
2023-10-17 17:11:11 +0200

    replace hardcoded values with constants

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 
+import constants as c
 
 if __name__ == "__main__":
     pygame.init()
-    pygame.display.set_mode((400, 500))
+    pygame.display.set_mode((c.WINDOW_WIDTH, c.WINDOW_HEIGHT))
     pygame.display.set_caption('Snake Reinforcement Learning')
     
     running = True
