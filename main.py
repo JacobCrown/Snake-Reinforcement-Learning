e2c016ac6455724bc463a83d9a9cf00c7d7cfbe1
JacobCrown <jkorona.pl@gmail.com>
2023-10-17 17:05:05 +0200

    game starts

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_mode((400, 500))
+    pygame.display.set_caption('Snake Reinforcement Learning')
+    
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
