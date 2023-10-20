7f319915dbfe65316b50f6df67208dc3b534f8c7
JacobCrown <jkorona.pl@gmail.com>
2023-10-20 19:09:35 +0200

    Add agent

---
+++ agent.py
@@ -0,0 +1,135 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import Game
+from constants import Direction, Point
+from model import Linear_QNet, QTrainer
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 
+        self.gamma = 0.9 
+        self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = Linear_QNet(12, 112, 4)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+
+    def get_state(self, game: Game):
+        snake = game.snake
+        head = snake.head
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+        
+        dir_l = snake.direction == Direction.LEFT
+        dir_r = snake.direction == Direction.RIGHT
+        dir_u = snake.direction == Direction.UP
+        dir_d = snake.direction == Direction.DOWN
+
+        state = [
+            # Danger up
+            snake.is_collision(point_u),
+
+            # Danger down
+            snake.is_collision(point_d),
+
+            # Danger right
+            snake.is_collision(point_r),
+
+            # Danger left
+            snake.is_collision(point_l),
+            
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # Food location 
+            game.apple.x < snake.head.x,  # food left
+            game.apple.x > snake.head.x,  # food right
+            game.apple.y < snake.head.y,  # food up
+            game.apple.y > snake.head.y  # food down
+            ]
+
+        return torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+
+    def remember(self, state, action, reward, next_state, game_over):
+        self.memory.append((state, action, reward, next_state, game_over)) # popleft if MAX_MEMORY is reached
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, game_overs = zip(*mini_sample)
+        state = torch.concat(states, axis=0)
+        next_state = torch.cat(next_states, axis=0)
+        action = torch.tensor(actions, dtype=torch.long)
+        reward = torch.tensor(rewards, dtype=torch.float32)
+
+        self.trainer.train_step(state, action, reward, next_state, game_overs)
+
+    def train_short_memory(self, state: torch.Tensor, action: int, reward: int,
+                           next_state: torch.Tensor, game_over: bool):
+        action_t = torch.tensor(action, dtype=torch.long).unsqueeze(0)
+        reward_t = torch.tensor(reward, dtype=torch.float32).unsqueeze(0)
+        next_state_t = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0)
+        state_t = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+        game_over_t = (game_over,)
+        
+        self.trainer.train_step(state_t, action_t, reward_t, next_state_t, game_over_t)
+
+    def get_action(self, state) -> Direction:
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games
+        move = 0
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 3)
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+
+        return Direction(move)
+
+
+def train():
+    record = 0
+    agent = Agent()
+    game = Game()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, game_over, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move.value, reward, state_new, game_over)
+
+        # remember
+        agent.remember(state_old, final_move.value, reward, state_new, game_over)
+
+        if game_over:
+            # train long memory, plot result
+            game = Game()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+if __name__ == '__main__':
+    train()
