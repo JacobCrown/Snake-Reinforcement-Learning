4e8c9942c42810995a8aabc8ada28619c7b1adf1
JacobCrown <jkorona.pl@gmail.com>
2023-10-20 16:23:53 +0200

    add QTrainer

--- model.py
+++ model.py
@@ -16,3 +16,35 @@ class Linear_QNet(nn.Module):
         x = F.relu(self.linear2(x))
         x = self.linear3(x)
         return x
+
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+    def train_step(self, state, action, reward, next_state, game_over):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx, go in enumerate(game_over):
+            Q_new = reward[idx]
+            if not go:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+    
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
