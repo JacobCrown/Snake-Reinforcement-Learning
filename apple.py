41f244fe573ae9b5e477ac0eb3d25bee766d61a7
JacobCrown <jkorona.pl@gmail.com>
2023-10-25 16:20:23 +0200

    simplify apple.py file

--- apple.py
+++ apple.py
@@ -1,17 +1,12 @@
-import random
-
 import pygame
 
-from constants import WINDOW_HEIGHT, WINDOW_WIDTH, BLOCK_SIZE, APPLE_COLOR
+from constants import BLOCK_SIZE, APPLE_COLOR
 
 
 class Apple():
     def __init__(self):
-        self.reset_apple()
+        self.x = 0
+        self.y = 0
 
-    def reset_apple(self):
-        self.x = random.randrange(0, WINDOW_WIDTH-1, BLOCK_SIZE)
-        self.y = random.randrange(0, WINDOW_HEIGHT-1, BLOCK_SIZE)
-        
     def draw(self, surf: pygame.Surface):
         pygame.draw.rect(surf, APPLE_COLOR, (self.x, self.y, BLOCK_SIZE, BLOCK_SIZE), 2)
