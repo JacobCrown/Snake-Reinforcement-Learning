bb488222d6a98be77cc98799fc7d7b90d941a7d8
JacobCrown <jkorona.pl@gmail.com>
2023-10-26 18:53:05 +0200

    Add BLOCK_WIDTH and BLOCK_HEIGHT constant values

--- constants.py
+++ constants.py
@@ -4,18 +4,20 @@ import enum
 from collections import namedtuple
 from pathlib import Path
 
-
-WINDOW_WIDTH = 320
-WINDOW_HEIGHT = 320
+BOARD_WIDTH = 20
+BOARD_HEIGHT = 20
 
 BLOCK_SIZE = 20
+
+WINDOW_WIDTH = int(BOARD_WIDTH * BLOCK_SIZE)
+WINDOW_HEIGHT = int(BOARD_HEIGHT * BLOCK_SIZE)
 
 SNAKE_COLOR = (0, 240, 0)
 APPLE_COLOR = (240, 0, 0)
 BACKGROUND_COLOR = (0, 0, 0)
 TEXT_COLOR = (0, 0, 240)
 
-FPS = 60
+FPS = 20
 
 MODELS_DIRPATH = Path(".models")
 
