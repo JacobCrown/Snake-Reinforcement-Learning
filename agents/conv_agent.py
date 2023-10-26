d30493df894b9dadcb50128635b83e07ddb7dc49
JacobCrown <jkorona.pl@gmail.com>
2023-10-26 22:25:24 +0200

    Overengineer Conv model

--- agents/conv_agent.py
+++ agents/conv_agent.py
@@ -8,12 +8,12 @@ from game import Game
 
 
 class ConvAgent(AgentInterface):
-    MAX_MEMORY = 10_000
-    BATCH_SIZE = 100
+    MAX_MEMORY = 1_000_000
+    BATCH_SIZE = 10_000
     LR = 1e-4
     GAMMA = 0.99
     INPUT_DIMS = (1, c.BOARD_BLOCK_WIDTH, c.BOARD_BLOCK_HEIGHT)
-    MODEL = Conv_QNet(1, 1, c.BLOCK_SIZE, c.BLOCK_SIZE, 0, 3)
+    MODEL = Conv_QNet(INPUT_DIMS, 3)
     SAVE_MODEL_NAME = "conv_model.pt"
     LOAD_MODEL = False
 
--- models/conv_model.py
+++ models/conv_model.py
@@ -1,19 +1,32 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class Conv_QNet(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, 
-                 output_size):
+    def __init__(self, in_dims: (int, int, int), num_actions: int):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        in_channels, h, w = in_dims
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
         self.flatten = nn.Flatten()
-        self.linear = nn.Linear(out_channels, output_size)
+        self.fc1 = nn.Linear(64 * (h-6) * (w-6), 128)  # Adjust the input size if needed
+        self.fc2 = nn.Linear(128, num_actions)
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv(x))
+        x = F.leaky_relu(self.conv1(x))
+        x = F.leaky_relu(self.conv2(x))
+        x = F.leaky_relu(self.conv3(x))
         x = self.flatten(x)
-        x = self.linear(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
         return x
 
 
+
+m = Conv_QNet((1, 20, 20), 3)
+
+t = torch.randn((100, 1, 20, 20))
+
+m(t)
